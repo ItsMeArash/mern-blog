9d691cdfd292926794bf9e033abe4ad55f6042d7
Arash Harandehi <56218691+ItsMeArash@users.noreply.github.com>
2024-06-17 11:37:28 +0330

    initial routes definition

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import Navbar from "./components/navbar.component.jsx";
+import {Route, Routes} from "react-router-dom";
 
 const App = () => {
     return (
-        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Navbar/>}>
+                <Route path="signin" element={<h1>SIGNIN</h1>}/>
+                <Route path="signup" element={<h1>SIGNUP</h1>}/>
+            </Route>
+        </Routes>
     )
 }
 
