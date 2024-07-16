724f826a26470fa9e6a3b57bf43517de70523257
Arash Harandehi <56218691+ItsMeArash@users.noreply.github.com>
2024-07-16 17:14:18 +0330

    trending blogs fetching route designed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx';
 import './index.css';
 import {BrowserRouter} from "react-router-dom";
 import "./i18n.js";
+import Loader from "./components/loader.component.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-        <React.Suspense fallback="loading...">
+        <React.Suspense fallback={<Loader/>}>
             <BrowserRouter>
                 <App/>
             </BrowserRouter>
--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -1,18 +1,52 @@
 import AnimationWrapper from "../common/page-animation.jsx";
 import InPageNavigation from "../components/inpage-navigation.component.jsx";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import Loader from "../components/loader.component.jsx";
+import BlogPostCard from "../components/blog-post.component.jsx";
 
 const HomePage = () => {
+    const [blogs, setBlogs] = useState(null);
+
+    const fetchLatestBlogs = () => {
+        axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+            .then((response) => {
+                setBlogs(response.data.blogs);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    useEffect(() => {
+        fetchLatestBlogs();
+    }, []);
+
+    useEffect(() => {
+        console.log(blogs)
+    }, [blogs]);
+
     return (
         <AnimationWrapper>
             <section className="h-cover flex justify-center gap-10">
                 {/*Latest blogs*/}
                 <div className="w-full">
                     <InPageNavigation routes={["home", "trending blogs"]} defaultHidden={["trending blogs"]}>
-                        <h1>Home Heading</h1>
+                        <>
+                            {
+                                blogs === null ? <Loader/> : (
+                                    blogs?.map((blog, index) => (
+                                        <AnimationWrapper key={index} transition={{duration: 1, delay: index * 0.1}}>
+                                            <BlogPostCard content={blog} author={blog.author.personal_info}/>
+                                        </AnimationWrapper>
+                                    ))
+                                )
+                            }
+                        </>
                         <h1>Trending Heading</h1>
                     </InPageNavigation>
                 </div>
-                {/*dFilters and trending blogs*/}
+                {/*Filters and trending blogs*/}
                 <div className="">
 
                 </div>
