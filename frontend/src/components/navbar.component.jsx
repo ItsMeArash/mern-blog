953f996f54a4c66cca89e001932b454e67bd1713
Arash Harandehi <56218691+ItsMeArash@users.noreply.github.com>
2024-08-01 02:17:12 +0330

    fix search bar visibility issue

--- frontend/src/components/navbar.component.jsx
+++ frontend/src/components/navbar.component.jsx
@@ -35,19 +35,20 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="navbar flex justify-between w-full">
+            <nav className="navbar flex justify-between w-full h-full">
                 <div className="flex items-center gap-2">
                     <Link to="/" className="w-10 h-10">
                         <img src={logo} alt="logo"/>
                     </Link>
                     <div
-                        className={`${searchBoxVisibility ? 'show' : 'hide'} md:show absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto`}>
+                        className={`search-box ${searchBoxVisibility ? 'show' : 'hide'} md:show absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto`}>
                         <input type="text"
                                placeholder={t("input.search")}
                                className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
-                                onKeyDown={handleSearch}/>
+                               onKeyDown={handleSearch}/>
                         <i className={`fi fi-rr-search absolute ${i18n.language === "fa" ? "left" : "right"}-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey`}/>
                     </div>
+                    <div className={`search-overlay ${searchBoxVisibility ? 'show' : ''}`} onClick={() => setSearchBoxVisibility(false)}></div>
                 </div>
                 <div className="flex items-center gap-3 md:gap-6">
                     <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"
