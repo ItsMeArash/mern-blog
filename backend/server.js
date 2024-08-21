ebcee1c75a6fbede390350d09e0d3598d4f734ba
Arash Harandehi <arashh7998@gmail.com>
2024-08-21 14:53:43 +0330

    storing comments on the database

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ import multer from "multer"
 import User from "./Schema/User.js";
 import Blog from "./Schema/Blog.js";
 import Notification from "./Schema/Notification.js";
+import Comment from "./Schema/Comment.js";
 import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
 
 const server = express();
@@ -494,6 +495,44 @@ server.post("/isliked-by-user", verifyJWT, (req, res) => {
         .catch(err => {
             return res.status(500).json({error: err.message});
         })
+})
+
+server.post("/add-comment", verifyJWT, (req, res) => {
+    const user_id = req.user;
+    const {_id, comment, blog_author} = req.body;
+
+    if (!comment.length) {
+        return res.status(403).json({error: "Write something to submit your comment!"});
+    }
+
+    const commentObject = new Comment({
+        blog_id: _id,
+        blog_author,
+        comment,
+        commented_by: user_id,
+    });
+    commentObject?.save().then(commentFile => {
+        const {comment, commentedAt, children} = commentFile;
+
+        Blog.findOneAndUpdate({_id}, {
+            $push: {"comments": commentFile._id},
+            $inc: {"activity.total_comments": 1},
+            "activity.total_parent_comments": 1
+        }).then(blog => {
+            console.log('edafufuiadsfasfhuvfCOMMMMEMEENNTaseuprbvgau')
+        })
+
+        const notificationObject = {
+            type: "comment",
+            blog: _id,
+            notification_for: blog_author,
+            user: user_id,
+            comment: commentFile._id
+        };
+        new Notification(notificationObject).save().then(notification => console.log("New notification created!"));
+
+        return res.status(200).json({comment, commentedAt, _id: commentFile._id, user_id, children});
+    })
 })
 
 server.listen(PORT, () => {
--- frontend/src/components/comment-field.component.jsx
+++ frontend/src/components/comment-field.component.jsx
@@ -1,7 +1,60 @@
-import {useState} from "react";
+import {useContext, useState} from "react";
+import {UserContext} from "../App.jsx";
+import {toast} from "react-hot-toast";
+import axios from "axios";
+import {BlogContext} from "../pages/blog.page.jsx";
 
 const CommentField = ({action}) => {
     const [comment, setComment] = useState('');
+
+    const {userAuth: {accessToken, username, fullname, profile_img}} = useContext(UserContext);
+    const {
+        blog,
+        blog: {_id, author: {_id: blog_author}, comments, activity, activity: {total_comments, total_parent_comments}},
+        setBlog,
+        setTotalParentCommentsLoaded
+    } = useContext(BlogContext);
+
+    const handleComment = () => {
+        if (!accessToken) {
+            return toast.error("Login to leave a comment!");
+        }
+        if (!comment.length) {
+            return toast.error("Empty comment can't be saved!");
+        }
+
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/add-comment", {_id, blog_author, comment}, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        })
+            .then(({data}) => {
+                setComment('');
+                data.commentedBy = {personal_info: {username, profile_img, fullname}};
+                let newCommentArray;
+
+                data.childrenLevel = 0;
+
+                newCommentArray = [data];
+
+                let parentCommentIncrementValue = 1;
+                setBlog({
+                    ...blog,
+                    comments: {...comments, results: newCommentArray},
+                    activity: {
+                        ...activity,
+                        total_comments: total_comments + 1,
+                        total_parent_comments: total_parent_comments + parentCommentIncrementValue
+                    }
+                })
+                setTotalParentCommentsLoaded(prev => prev + parentCommentIncrementValue);
+
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+    };
 
     return (
         <>
@@ -10,7 +63,10 @@ const CommentField = ({action}) => {
                       placeholder="Leave a comment..."
                       className="input-box pl-5 placeholder:text-dark-grey resize-none h-[150px] overflow-auto">
             </textarea>
-            <button className="btn-dark mt-5 px-10">{action}</button>
+            <button className="btn-dark mt-5 px-10"
+                    onClick={handleComment}>
+                {action}
+            </button>
         </>
     );
 };
